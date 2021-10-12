e5b2227d7c3b7532441a2029e8d9f311e6d83e05
Rick(Rugui) Chen <rickruguichen@google.com>
2021-10-12 14:45:10 -0400

    evtsubst

--- sky-projects/scripts/deploy-project.sh
+++ sky-projects/scripts/deploy-project.sh
@@ -5,12 +5,10 @@ export billing_account_id=011552-7DA53C-81003E
 export projects_namespace=projects
 export management_project_id=rick-multi-tenancy
 export management_namespace=config-control
-
-
 cd $deployment_dir
 kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/base@main ./$project_id
 cd $project_id
-envsubst < "setters.yaml.template" >  "setters.yaml"
+envsubst < "./setters.yaml.template" >  "setters.yaml"
 kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/bigquery@main ./bigquery
 cd bigquery
 export data_admin_group="data-admin@rickruguichen.altostrat.com"
@@ -19,10 +17,10 @@ export data_viewer_group="data-viewer@rickruguichen.altostrat.com"
 export data_job_group="data-job@rickruguichen.altostrat.com"
 export bq_dataset_name=bqsampledataset
 export bq_datatable_name=bqsampledatatable
-envsubst < "setters.yaml.template" >  "setters.yaml"
+envsubst < "./setters.yaml.template" >  "setters.yaml"
 cd $deployment_dir
 git add .
-git commit -m "project id: $project_id"
+git commit -m "create project project id: $project_id"
 git push
 
 

9a685fdaadc36eb1a82a04226723c6ec17803e40
Rick(Rugui) Chen <rickruguichen@google.com>
2021-10-12 10:54:26 -0400

    deployment script

---
+++ sky-projects/scripts/deploy-project.sh
@@ -0,0 +1,17 @@
+deployment_dir=$HOME/source-repo/sky-projects
+project-id=sky-data-$RANDOM
+cd $deployment_dir
+kpt pkg get https://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@github.com/rick-c-goog/sky_deployment/sky-projects/base@main ./$project_id
+kpt cfg set $project-id/ project-id $project-id
+kpt cfg set $project-id/ billing-account-id 011552-7DA53C-81003E
+kpt cfg set $project-id/ projects-namespace projects
+kpt cfg set $project-id/ project-namespace $project-id
+kpt cfg set $project-id/ management-project-id rick-multi-tenancy
+kpt cfg set $project-id/ management-namespace config-control 
+cd $project-id
+kpt pkg get https://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@github.com/rick-c-goog/sky_deployment/sky-projects/bigquery@main ./bigquery
+kpt cfg set bigquery/ data-admin-group "data-admin@rickruguichen.altostrat.com"
+kpt cfg set bigquery/ data-editor-group "data-editor@rickruguichen.altostrat.com"
+kpt cfg set bigquery/ data-viewer-group "data-viewer@rickruguichen.altostrat.com"
+kpt cfg set bigquery/ data-job-group "data-job@rickruguichen.altostrat.com"
+

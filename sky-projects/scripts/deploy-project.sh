aabe6541057900edea3d9e9f8a8dbbf0a8fc6ab4
Rick(Rugui) Chen <rickruguichen@google.com>
2021-10-12 11:23:32 -0400

    deployment script

--- sky-projects/scripts/deploy-project.sh
+++ sky-projects/scripts/deploy-project.sh
@@ -2,14 +2,14 @@ deployment_dir=$HOME/source-repo/sky-projects
 project_id=sky-data-$RANDOM
 cd $deployment_dir
 kpt pkg get https://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@github.com/rick-c-goog/sky_deployment/sky-projects/base@main ./$project_id
-kpt cfg set $project-id/ project-id $project-id
-kpt cfg set $project-id/ sky-team-name sky-test
-kpt cfg set $project-id/ billing-account-id 011552-7DA53C-81003E
-kpt cfg set $project-id/ projects-namespace projects
-kpt cfg set $project-id/ project-namespace $project-id
-kpt cfg set $project-id/ management-project-id rick-multi-tenancy
-kpt cfg set $project-id/ management-namespace config-control 
-cd $project-id
+kpt cfg set $project_id/ project-id $project_id
+kpt cfg set $project_id/ sky-team-name sky-test
+kpt cfg set $project_id/ billing-account-id 011552-7DA53C-81003E
+kpt cfg set $project_id/ projects-namespace projects
+kpt cfg set $project_id/ project-namespace $project_id
+kpt cfg set $project_id/ management-project-id rick-multi-tenancy
+kpt cfg set $project_id/ management-namespace config-control 
+cd $project_id
 kpt pkg get https://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@github.com/rick-c-goog/sky_deployment/sky-projects/bigquery@main ./bigquery
 kpt cfg set bigquery/ data-admin-group "data-admin@rickruguichen.altostrat.com"
 kpt cfg set bigquery/ data-editor-group "data-editor@rickruguichen.altostrat.com"
@@ -18,6 +18,6 @@ kpt cfg set bigquery/ data-job-group "data-job@rickruguichen.altostrat.com"
 
 cd $deployment_dir
 git add .
-git commit -m "project id: $project-id"
+git commit -m "project id: $project_id"
 git push
 

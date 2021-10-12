357d921a7d74f50bac751816f7b4fa0fc860e52f
Rick(Rugui) Chen <rickruguichen@google.com>
2021-10-12 17:11:35 -0400

    evtsubst

--- sky-projects/scripts/deploy-project.sh
+++ sky-projects/scripts/deploy-project.sh
@@ -1,5 +1,5 @@
 deployment_dir=$HOME/source-repo/sky-projects
-export project_id=sky-data-prj5
+export project_id=sky-data$RANDOM
 export sky_team_name=sky-test
 export billing_account_id=011552-7DA53C-81003E
 export projects_namespace=projects
@@ -16,21 +16,21 @@ git add .
 git commit -m "create project project id: $project_id stage 1"
 git push
 
-#sleep 60
-#cd $deployment_dir/$project_id
-#kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/bigquery@main ./bigquery
-#cd bigquery
-#export data_admin_group="data-admin@rickruguichen.altostrat.com"
-#export data_editor_group="data-editor@rickruguichen.altostrat.com"
-#export data_viewer_group="data-viewer@rickruguichen.altostrat.com"
-#export data_job_group="data-job@rickruguichen.altostrat.com"
-#export bq_dataset_name=bqsampledataset
-#export bq_datatable_name=bqsampledatatable
-#envsubst < "./setters.yaml.template" >  "setters.yaml"
-#cd $deployment_dir
-#git add .
-#git commit -m "create project project id: $project_id"
-#git push
+sleep 160
+cd $deployment_dir/$project_id
+kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/bigquery@main ./bigquery
+cd bigquery
+export data_admin_group="data-admin@rickruguichen.altostrat.com"
+export data_editor_group="data-editor@rickruguichen.altostrat.com"
+export data_viewer_group="data-viewer@rickruguichen.altostrat.com"
+export data_job_group="data-job@rickruguichen.altostrat.com"
+export bq_dataset_name=bqsampledataset
+export bq_datatable_name=bqsampledatatable
+envsubst < "./setters.yaml.template" >  "setters.yaml"
+cd $deployment_dir
+git add .
+git commit -m "create project project id: $project_id"
+git push
 
 
 

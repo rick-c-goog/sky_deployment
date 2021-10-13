435777f5bf6869904b31eb59f2655959f6781284
Rick(Rugui) Chen <rickruguichen@google.com>
2021-10-12 23:53:13 -0400

    cleanup

---
+++ sky-projects/scripts/deploy-data-project.sh
@@ -0,0 +1,34 @@
+deployment_dir=$HOME/source-repo/sky-projects
+export project_id=sky-data-$RANDOM
+export sky_team_name=sky-test
+export billing_account_id=011552-7DA53C-81003E
+export projects_namespace=projects
+export project_namespace=$project_id
+export projects_namespace=projects
+export management_project_id=rick-multi-tenancy
+export management_namespace=config-control
+cd $deployment_dir
+kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/base@main ./$project_id
+cd $project_id
+envsubst < "./setters.yaml.template" >  "setters.yaml"
+
+
+kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/bigquery@main ./bigquery
+cd bigquery
+export data_admin_group="data-admin@rickruguichen.altostrat.com"
+export data_editor_group="data-editor@rickruguichen.altostrat.com"
+export data_viewer_group="data-viewer@rickruguichen.altostrat.com"
+export data_job_group="data-job@rickruguichen.altostrat.com"
+export bq_dataset_name=bqsampledataset
+export bq_datatable_name=bqsampledatatable
+envsubst < "./setters.yaml.template" >  "setters.yaml"
+cd $deployment_dir
+git add .
+git commit -m "create project project id: $project_id"
+git push
+
+export project_id=sky-data$RANDOM
+
+
+
+
--- sky-projects/scripts/deploy-slot-project.sh
+++ sky-projects/scripts/deploy-slot-project.sh
@@ -1,5 +1,5 @@
 deployment_dir=$HOME/source-repo/sky-projects
-export project_id=sky-data$RANDOM
+export project_id=sky-slot-$RANDOM
 export sky_team_name=sky-test
 export billing_account_id=011552-7DA53C-81003E
 export projects_namespace=projects
@@ -7,25 +7,30 @@ export project_namespace=$project_id
 export projects_namespace=projects
 export management_project_id=rick-multi-tenancy
 export management_namespace=config-control
+
+export data_project_id=XXXXXXX # replace data project reference
+export data_job_group="data-job@rickruguichen.altostrat.com"
+export data_reservation_group="data-reservation@rickruguichen.altostrat.com"
+export bq_dataset_name=bqsampledataset
+export bq_datatable_name=bqsampledatatable
+
 cd $deployment_dir
 kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/base@main ./$project_id
 cd $project_id
 envsubst < "./setters.yaml.template" >  "setters.yaml"
 
 
-kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/bigquery@main ./bigquery
-cd bigquery
-export data_admin_group="data-admin@rickruguichen.altostrat.com"
-export data_editor_group="data-editor@rickruguichen.altostrat.com"
-export data_viewer_group="data-viewer@rickruguichen.altostrat.com"
-export data_job_group="data-job@rickruguichen.altostrat.com"
-export bq_dataset_name=bqsampledataset
-export bq_datatable_name=bqsampledatatable
+kpt pkg get https://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@github.com/rick-c-goog/sky_deployment.git/sky-projects/bigquery@main ./job
+cd job
+
 envsubst < "./setters.yaml.template" >  "setters.yaml"
 cd $deployment_dir
 git add .
 git commit -m "create project project id: $project_id"
 git push
+
+export project_id=sky-data$RANDOM
+
 
 
 

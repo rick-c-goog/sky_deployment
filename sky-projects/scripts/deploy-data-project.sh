f0ba4adc7b1db1dc9d612acf34e7f54fd073cce6
Rick(Rugui) Chen <rickruguichen@google.com>
2021-10-15 17:20:45 -0400

    name conventions cleanup

--- sky-projects/scripts/deploy-data-project.sh
+++ sky-projects/scripts/deploy-data-project.sh
@@ -4,6 +4,7 @@ export sky_team_name=sky-test
 export billing_account_id=011552-7DA53C-81003E
 export project_namespace=$project_id
 export projects_namespace=projects
+export management_project_id=rick-multi-tenancy
 export system_admin_group="gcp-org-admins@rickruguichen.altostrat.com"
 source_repo=https://$githubtoken@github.com/rick-c-goog/sky_deployment.git
 cd $deployment_dir

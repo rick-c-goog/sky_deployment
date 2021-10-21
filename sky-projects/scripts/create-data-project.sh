ab9d87ec38c34215c2eafc374d80d4943fb90179
Rick(Rugui) Chen <rickruguichen@google.com>
2021-10-21 13:31:37 -0400

    folder_id

--- sky-projects/scripts/create-data-project.sh
+++ sky-projects/scripts/create-data-project.sh
@@ -2,6 +2,7 @@
 deployment_dir=$HOME/source-repo/sky-projects
 export project_id=sky-data-$RANDOM
 export sky_team_name=sky-credit-risk
+export sky_team_folder_id=342025906281
 export billing_account_id=XXXX-XXX-XXX
 export project_namespace=sky-credit-risk
 export projects_namespace=projects
